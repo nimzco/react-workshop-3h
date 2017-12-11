e6b217a76a4f9cdf2b215d3d77372d29f910c356
Nima Izadi <nima.izadi@shopify.com>
2017-12-11 15:17:45 -0500

    Remove unused things

--- src/exercise1/index.js
+++ src/exercise1/index.js
@@ -1,13 +1,16 @@
 import React from "react";
+import ReactDOM from "react-dom";
 
 function heading({ value }) {
   return <h1>{value}</h1>;
 }
 
-export default (
+const element = (
   <div className="container">
     {heading({ value: "Hello World" })}
     {heading({ value: "How are you" })}
   </div>
 );
 
+
+ReactDOM.render(element, document.getElementById('root'));
--- src/index.js
+++ src/index.js
@@ -1,2 +1,2 @@
 import './index.css';
-import './exercise2';
+import './exercise1';
