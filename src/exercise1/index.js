83db6c8b179c344fc7c75eb8c5eb739fa6ad16a4
Claudéric Demers <clauderic.demers@shopify.com>
2017-12-12 10:47:51 -0500

    Update first lecture to add exercices for DOM / createElement / JSX

--- src/exercise1/index.js
+++ src/exercise1/index.js
@@ -1,9 +1,6 @@
 // -------------------------------------------------------------------------- //
-// Ex. 1 Rendering in JSX
+// Ex. 1 Rendering elements using plain JavaScript
 // -------------------------------------------------------------------------- //
-
-import React from "react";
-import ReactDOM from "react-dom";
 
 const rootNode = document.getElementById('root');
 const element = document.createElement('div');
@@ -13,16 +10,58 @@ element.className = 'container';
 
 rootNode.appendChild(element);
 
-function heading({ value }) {
-  return <h1>{value}</h1>;
-}
 
-const element2 = (
+// -------------------------------------------------------------------------- //
+// Ex. 2 Rendering elements using React.createElement(type, props, children)
+// -------------------------------------------------------------------------- //
+
+// import React from "react";
+// import ReactDOM from "react-dom";
+
+// const element = React.createElement(
+//   'div',
+//   {className: 'container'},
+//   'Hello World',
+// );
+
+// ReactDOM.render(element, document.getElementById('root'));
+
+
+// -------------------------------------------------------------------------- //
+// Ex. 3 Rendering elements using JSX
+//
+// We can verify that the code below is equivalent to the previous exercise by
+// checking out the output produced by Babel: https://goo.gl/64yWVP
+// -------------------------------------------------------------------------- //
+
+import React from "react";
+import ReactDOM from "react-dom";
+
+const element = (
   <div className="container">
-    {heading({ value: "Hello World" })}
-    {heading({ value: "How are you" })}
+    Hello World
   </div>
 );
 
+ReactDOM.render(element, document.getElementById('root'));
 
-ReactDOM.render(element2, document.getElementById('root'));
+
+// -------------------------------------------------------------------------- //
+// Ex. 4 Embedding expressions in JSX
+// -------------------------------------------------------------------------- //
+
+// import React from "react";
+// import ReactDOM from "react-dom";
+
+// function heading({ value }) {
+//   return <h1>{value}</h1>;
+// }
+
+// const element2 = (
+//   <div className="container">
+//     {heading({value: 'Hello World'})}
+//     {heading({value: 'How are you'})}
+//   </div>
+// );
+
+// ReactDOM.render(element2, document.getElementById('root'));
