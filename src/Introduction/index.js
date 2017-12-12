909a76e5ed4bc8e48d3e35ebfe8440bc64d5fac3
Claudéric Demers <clauderic.demers@shopify.com>
2017-12-12 17:21:58 -0500

    Open intro sandbox in a new page

--- src/Introduction/index.js
+++ src/Introduction/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 export default () => (
-  <a href="https://codesandbox.io/s/j7wn0xnp29">
+  <a href="https://codesandbox.io/s/j7wn0xnp29" target="_blank">
     Go to Introduction sandbox
   </a>
 );
