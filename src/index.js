ed578e3a782c7dbfeea2697ced8dd65e44743bba
Claudéric Demers <clauderic.demers@shopify.com>
2017-12-12 17:17:14 -0500

    Comment out Lists exercise for now

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ const routes = [
   {id: 'events-state', label: 'Events & SetState', component: require('./Events-set-state').default, showSolution: true},
   {id: 'lifecycle-mount', label: 'Lifecycle methods 1', component: require('./Lifecycle-mount').default, showSolution: true},
   {id: 'lifecycle-unmount', label: 'Lifecycle methods 2', component: require('./Lifecycle-unmount').default, showSolution: true},
-  {id: 'lists', label: 'Lists', component: require('./Lists').default, showSolution: true},
+  // {id: 'lists', label: 'Lists', component: require('./Lists').default, showSolution: true},
   {id: 'lifting-state-up', label: 'Lifting State Up', component: require('./Lifting-state-up').default, showSolution: true},
 ];
 
