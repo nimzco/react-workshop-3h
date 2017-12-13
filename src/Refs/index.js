65618caa4029b344ddf7d983d4b8b1c6ca04f1b0
Claudéric Demers <clauderic.demers@shopify.com>
2017-12-12 20:38:01 -0500

    Add Refs exercise

---
+++ src/Refs/index.js
@@ -0,0 +1,17 @@
+// -------------------------------------------------------------------------- //
+// Using Refs to access DOM Nodes
+// -------------------------------------------------------------------------- //
+
+import React from "react";
+
+export default class Exercise extends React.Component {
+  render() {
+    return (
+      <div>
+        <input />
+        <button>Focus input</button>
+      </div>
+    );
+  }
+}
+
--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@ const routes = [
   {id: 'lifecycle-mount', label: 'Lifecycle methods 1', component: require('./Lifecycle-mount').default, showSolution: true},
   {id: 'lifecycle-unmount', label: 'Lifecycle methods 2', component: require('./Lifecycle-unmount').default, showSolution: true},
   // {id: 'lists', label: 'Lists', component: require('./Lists').default, showSolution: true},
+  {id: 'refs', label: 'Using Refs', component: require('./Refs').default, showSolution: true},
   // {id: 'lifting-state-up', label: 'Lifting State Up', component: require('./Lifting-state-up').default, showSolution: true},
 ];
 
