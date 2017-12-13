3064682679b872d9fd657f8dddc63a4320253113
Nima Izadi <nima.izadi@shopify.com>
2017-12-13 09:03:47 -0500

    Removed solutions

--- src/Lifecycle-unmount/index.js
+++ src/Lifecycle-unmount/index.js
@@ -3,7 +3,7 @@
 // -------------------------------------------------------------------------- //
 
 import React from "react";
-import Timer from "./Timer.solution";
+import Timer from "./Timer";
 
 export default class App extends React.Component {
   state = { showTimer: false };
