5b3046ef8133c34d1d6cd9806e3d8dae8a877d22
Nima Izadi <nima.izadi@shopify.com>
2017-12-14 11:01:24 -0500

    Updated folder names

--- src/index.js
+++ src/index.js
@@ -7,13 +7,12 @@ const routes = [
   {id: 'introduction', label: '0 - Introduction', component: require('./0-Introduction').default},
   {id: 'events-state', label: '1 - Events set state', component: require('./1-Events-set-state').default, showSolution: true},
   {id: 'lifecycle-mount', label: '2 - Data fetching', component: require('./2-Data-fetching').default, showSolution: true},
-  {id: 'lists', label: '3 - Lists', component: require('./3-Lists').default, showSolution: true},
-  {id: 'refs', label: '4 - Refs and the DOM', component: require('./4-Refs-and-the-dom').default, showSolution: true},
-  {id: 'refs', label: '5 - Component Refs', component: require('./5-Component-refs').default},
-  {id: 'lifting-state-up', label: '6 - Communicating between components', component: require('./6-Component-communication').default, showSolution: true},
-  {id: 'forms', label: '7 - Forms', component: require('./7-Forms').default, showSolution: true},
-  {id: 'context', label: '8 - Context', component: require('./8-Context').default, showSolution: true},
-  {id: 'higher-order-components', label: '9 - Higher-order Components', component: require('./9-Higher-order-components').default},
+  {id: 'refs', label: '3 - Refs and the DOM', component: require('./3-Refs-and-the-dom').default, showSolution: true},
+  {id: 'refs-2', label: '4 - Component Refs', component: require('./4-Component-refs').default},
+  {id: 'lifting-state-up', label: '5 - Communicating between components', component: require('./5-Component-communication').default, showSolution: true},
+  {id: 'forms', label: '6 - Forms', component: require('./6-Forms').default, showSolution: true},
+  {id: 'context', label: '7 - Context', component: require('./7-Context').default, showSolution: true},
+  {id: 'higher-order-components', label: '8 - Higher-order Components', component: require('./8-Higher-order-components').default},
 ];
 
 ReactDOM.render(
