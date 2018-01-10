f2e57a341d414f52a6360d0201ea1ccf88d771d4
Claudéric Demers <clauderic.demers@shopify.com>
2018-01-10 11:23:46 -0500

    Update form example

--- src/5-Forms/index.js
+++ src/5-Forms/index.js
@@ -17,11 +17,13 @@ export default class Solution extends React.Component {
         <code>
           {JSON.stringify(this.state)}
         </code>
-
-        <input type="text" name="company" placeholder="Company" />
-        <input type="text" name="name" placeholder="Name" />
-        <textarea name="message" placeholder="message" />
-        <small>Number of characters remaining: ?</small>
+        <form>
+          <input type="text" name="company" placeholder="Company" />
+          <input type="text" name="name" placeholder="Name" />
+          <textarea name="message" placeholder="message" />
+          <small>Number of characters remaining: ?</small>
+          <button type="submit">Submit</button>
+        </form>
       </div>
     );
   }
