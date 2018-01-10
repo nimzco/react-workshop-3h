2d4388305ab69f23777ac3fb2d994f44fd380416
Claudéric Demers <clauderic.demers@shopify.com>
2018-01-10 10:56:58 -0500

    Update introduction

--- src/0-Introduction/index.js
+++ src/0-Introduction/index.js
@@ -2,7 +2,6 @@
 // Introduction to Rendering in react
 // -------------------------------------------------------------------------- //
 //  Objectives:
-// • Using the `React.createElement` API to render a basic Hello World example
 // • Understand how JSX is transpiled into React.createElement calls (using Babel)
 // • Recall the differences between HTML and JSX
 // • Learn how to embed expressions in JSX
@@ -11,15 +10,15 @@ import React from 'react';
 
 export default () => (
   <div>
-    <h1>Rendering in React</h1>
+    <h1>Introduction to JSX</h1>
     <p>
       For this introduction, we invite you to head over to a separate sandbox
       we have set up to walk you through the fundamentals of how to render
       elements in React.
     </p>
     <br/>
-    <a href="https://codesandbox.io/s/j7wn0xnp29" target="_blank">
-      <button>Go to Introduction sandbox</button>
+    <a href="https://codesandbox.io/s/n7o77v6k34" target="_blank">
+      <button>Go to JSX Exercises</button>
     </a>
   </div>
 );
